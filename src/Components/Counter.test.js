895343e30e8a6e4006c1ec9d2832ccd6f5ed759f
fatmadurak <fatmabadillidurak@gmail.com>
2023-01-31 11:44:00 +0300

    c

--- src/Components/Counter.test.js
+++ src/Components/Counter.test.js
@@ -1,3 +1,13 @@
 import {render, screen} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
+import Counter from "./Counter";
+
+
+test('increase btn', () => { 
+
+  render(<Counter/>)
+
+
+
+ })
